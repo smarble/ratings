755faf6f03eeaf4c73c1c9b3325c58f4693e42f1
Your Name <you@example.com>
2020-02-25 14:22:52 -0800

    playing with date formats, next is Autoincrement

--- seed.py
+++ seed.py
@@ -2,11 +2,13 @@
 
 from sqlalchemy import func
 from model import User
-# from model import Rating
-# from model import Movie
+from model import Rating
+from model import Movie
 
 from model import connect_to_db, db
 from server import app
+
+# def means these are functions for loading info into our detabase
 
 
 def load_users():
@@ -40,19 +42,31 @@ def load_movies():
 
     # Delete all rows in table, so if we need to run this a second time,
     # we won't be trying to add duplicate users
-    User.query.delete()
+    Movie.query.delete()
 
     # Read u.user file and insert data
     for row in open("seed_data/u.item"):
         row = row.rstrip()
-        movie_id, title, released_at, imdb_url = row.split("|")
 
-        user = User(user_id=user_id,
-                    age=age,
-                    zipcode=zipcode)
+        sliced_list = row.split("|")
+        movie_id = sliced_list[0]
+        title = sliced_list[1]
+        released_at = sliced_list[2]
+        imdb_url = sliced_list[3]
+    # in the variable below, we sliced title and removed the last 6 characters
+
+        title = title[:-6]
+
+    # another way of slicing the above information movie_id, title,
+    # released_at, imdb_url = row.split("|")
+
+        movie_info = Movie(movie_id=movie_id,
+                           title=title,
+                           released_at=released_at,
+                           imdb_url=imdb_url)
 
         # We need to add to the session or it won't ever be stored
-        db.session.add(user)
+        db.session.add(movie_info)
 
     # Once we're done, we should commit our work
     db.session.commit()
@@ -60,6 +74,28 @@ def load_movies():
 
 def load_ratings():
     """Load ratings from u.data into database."""
+
+    print("rating")
+
+    # Delete all rows in table, so if we need to run this a second time,
+    # Use the class name from model.py for the .query.delete() below.
+    Rating.query.delete()
+
+    # Read u.user file and insert data
+    for row in open("seed_data/u.data"):
+        row = row.rstrip()
+        rating_id, movie_id, user_id, score = row.split(" ")
+
+        rating_info = Rating(rating_id=rating_id,
+                             movie_id=movie_id,
+                             user_id=user_id,
+                             score=score)
+
+        # We need to add to the session or it won't ever be stored
+        db.session.add(rating_info)
+
+    # Once we're done, we should commit our work
+    db.session.commit()
 
 
 def set_val_user_id():
